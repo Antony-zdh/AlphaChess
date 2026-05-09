e8bbe349df6578558baccfee589501ea2047514d
Antony ZHAO <zdh22918@gmail.com>
2026-05-09 17:41:01 +0800

    Update inference environment.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 python src/main.py \
     --player0 AI \
     --player1 human \
-    --model_path models/alpha_chess_epoch_16.pth \
+    --model_path models/ppo_models/ppo_model_itr_20/ppo_model_itr_20.pth \
     --use_gui
